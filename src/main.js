767276233463a28d73aab71d036502da29603e41
ICELI <woticeli@gmail.com>
2017-07-26 11:44:01 +0800

    chore: echarts all done

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@
 
 const echarts = require('echarts');
 window.echarts = echarts;
-const world = require('./assets/js/world');
+const world = require('./assets/js/map/world');
+const china = require('./assets/js/map/china');
