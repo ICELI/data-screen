ec9a043b34a04d84dea5b72b5cc124688135768d
ICELI <woticeli@gmail.com>
2017-09-18 17:10:19 +0800

    refactor: remove cn code

--- src/App.vue
+++ src/App.vue
@@ -6,73 +6,7 @@
 
 <script>
   export default {
-    name: 'app',
-    data() {
-      return {
-        nextPage: '',
-        firstScreen: ['page1', 'page2', 'page3', 'page4'],
-        secondScreen: ['page5', 'page6', 'page7', 'page8']
-      };
-    },
-    components: {},
-    watch: {
-      $route() {
-        let times = 5;// 切屏时长 分钟
-        let isFirstScreen = this.firstScreen.indexOf(this.$route.name) > -1;
-        let currentPage = isFirstScreen ? this.firstScreen.indexOf(this.$route.name) : this.secondScreen.indexOf(this.$route.name);
-        this.nextPage = !isFirstScreen ? this.firstScreen[currentPage] : this.secondScreen[currentPage];
-        // 每屏的第一个窗口执行定时切换大屏任务
-        if(this.$route.name === this.firstScreen[0] || this.$route.name === this.secondScreen[0]) {
-          console.info(`======大屏定时切换主控页面======== ${this.$route.name} 至 ${this.nextPage}，间隔${times}分钟`);
-
-          this.timer = setTimeout(() => {
-            localStorage.setItem('isFirstScreen', localStorage.isFirstScreen === 'false' ? 'true' : 'false');
-            this.goNext(this.nextPage);
-          }, times * 60 * 1000);
-        }
-
-        console.log('watch $route', this.$route.name, this.nextPage);
-      },
-    },
-    computed: {},
-    methods: {
-      goNext(name) {
-        window.clearTimeout(window._time0);
-        window.clearTimeout(window._time7);
-        window.clearTimeout(window._time02);
-        window.clearTimeout(window._time);
-        window.clearTimeout(window._time2);
-        window.clearTimeout(window._time21);
-        window.clearTimeout(window._time3);
-        window.clearTimeout(window._time8);
-        this.$router.push(name);
-        clearTimeout(this.timer);
-        console.log('goNext', name);
-      }
-    },
-    created() {
-      console.dir(this.Api);
-    },
-    mounted() {
-      localStorage.isFirstScreen = this.firstScreen.indexOf(this.$route.name) > -1;
-
-//      console.log('isFirstScreen', localStorage.isFirstScreen, this.$route.name);
-
-      window.addEventListener('storage', (event) => {
-        console.log(event.key + "=" + event.newValue);
-        if (event.key === 'isFirstScreen') {
-          this.goNext(this.nextPage);
-        }
-      }, false);
-
-      document.addEventListener('keyup', (event) => {
-        event.preventDefault();
-        if (event.keyCode === 38 || event.keyCode === 40) {
-          localStorage.setItem('isFirstScreen', localStorage.isFirstScreen === 'false' ? 'true' : 'false');
-          this.goNext(this.nextPage);
-        }
-      });
-    }
+    name: 'app'
   };
 
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,10 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 // 引入路由视图，懒加载处理
-const home = () => import(/* webpackChunkName: "home" */ 'views/home');
-
 const page1 = () => import(/* webpackChunkName: "page1" */ 'views/panel-1-1');
 const page2 = () => import(/* webpackChunkName: "page2" */ 'views/panel-1-2');
 const page3 = () => import(/* webpackChunkName: "page3" */ 'views/panel-1-3');
 const page4 = () => import(/* webpackChunkName: "page4" */ 'views/panel-1-4');
-const page5 = () => import(/* webpackChunkName: "page5" */ 'views/panel-2-1');
-const page6 = () => import(/* webpackChunkName: "page6" */ 'views/panel-2-2');
-const page7 = () => import(/* webpackChunkName: "page7" */ 'views/panel-2-3');
-const page8 = () => import(/* webpackChunkName: "page8" */ 'views/panel-2-4');
 const page404 = () => import(/* webpackChunkName: "page404" */ 'views/page404');
 
 const router = new Router({
@@ -29,10 +23,7 @@ const router = new Router({
     {
       path: '/',
       name: 'home',
-      component: home,
-      meta: {
-        title: 'home'
-      }
+      redirect: 'page1'
     },
     {
       path: '/page1',
@@ -64,38 +55,6 @@ const router = new Router({
       component: page4,
       meta: {
         title: 'page4'
-      }
-    },
-    {
-      path: '/page5',
-      name: 'page5',
-      component: page5,
-      meta: {
-        title: 'page5'
-      }
-    },
-    {
-      path: '/page6',
-      name: 'page6',
-      component: page6,
-      meta: {
-        title: 'page6'
-      }
-    },
-    {
-      path: '/page7',
-      name: 'page7',
-      component: page7,
-      meta: {
-        title: 'page7'
-      }
-    },
-    {
-      path: '/page8',
-      name: 'page8',
-      component: page8,
-      meta: {
-        title: 'page8'
       }
     },
     {
